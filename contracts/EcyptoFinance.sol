4bd616ea4323cecd0fc12c17b5660f2c49a4a791
khade49 <khade4982@gmail.com>
2024-11-17 12:13:18 +0530

    staking contract changes

--- contracts/EcyptoFinance.sol
+++ contracts/EcyptoFinance.sol
@@ -18,7 +18,7 @@ interface IEcryptoToken {
         address to,
         uint256 amount
     ) external returns (bool);
-
+    
     function allowance(address owner, address spender)
         external
         view
